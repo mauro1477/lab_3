b7b5d9685018ecdbbb2bc61f2ef8eab0ed573d3a
mauro1477 <mava4370@colorado.edu>
2019-09-18 19:48:15 -0600

    Mauro's commit

---
+++ lab3_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Atuthors : Mauro Vargas Jr
+# Date: 09/18/2019
+
+#Problem 1 Code:
+#Make sure to document solving each problem!
+echo "Input a file name: "
+read filename
+erep '[0-9]\{3\}[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' filename
+
+
