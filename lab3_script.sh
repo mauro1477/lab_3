8dc734d302c70d331d0acb5a084739dedbc74168
mauro1477 <mava4370@colorado.edu>
2019-09-19 18:11:40 -0600

    new version of lab3_script

--- lab3_script.sh
+++ lab3_script.sh
@@ -4,8 +4,21 @@
 
 #Problem 1 Code:
 #Make sure to document solving each problem!
-echo "Input a file name: "
-read filename
-erep '[0-9]\{3\}[ -]\?[0-9]\{3\}[ -]\?[0-9]\{4\}' filename
+echo "Input regular expression and File Name: "
+read expression filename
+
+grep $expression $filename
+
+#count of the phone numbers
+grep -c -P '[0-9]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
+
+#list of all the phone numbers with '303'
+grep -o -P '303-[0-9]{3}-[0-9]{4}' regex_practice.txt
+
+#count of the emails
+grep -c -P '[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+' regex_practice.txt
+
+#store all the emails into the email_results.txt
+grep -o -P '[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+' regex_practice.txt >> email_results.txt 
 
 
